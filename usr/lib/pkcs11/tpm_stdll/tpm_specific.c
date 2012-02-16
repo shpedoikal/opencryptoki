ecc4cba1355a099df1a673026b2fd95e79aba9bd
Joy Latten <jmlatten@users.sourceforge.net>
2012-02-16 14:47:41 -0600

    Allow tpm stdll to get SRK passwd from OCK_SRK_SECRET and the policy mode for the secret from OCK_SRK_MODE env variables.
    Signed-off-by: Joy Latten <jmlatten@users.sourceforge.net>

--- usr/lib/pkcs11/tpm_stdll/tpm_specific.c
+++ usr/lib/pkcs11/tpm_stdll/tpm_specific.c
@@ -602,6 +602,7 @@ token_load_srk()
 	TSS_HPOLICY hPolicy;
 	TSS_RESULT result;
 	TSS_UUID SRK_UUID = TSS_UUID_SRK;
+	struct srk_info srk;
 
 	if (hSRK != NULL_HKEY)
 		return TSS_SUCCESS;
@@ -631,9 +632,16 @@ token_load_srk()
 	}
 #endif
 
-	if ((result = Tspi_Policy_SetSecret(hPolicy, TSS_SECRET_MODE_PLAIN, 0, NULL))) {
+	/* get the srk info */
+	memset(&srk, 0, sizeof(srk));
+	if (get_srk_info(&srk))
+		return -1;
+
+	if ((result = Tspi_Policy_SetSecret(hPolicy, (TSS_FLAG)srk.mode, srk.len, (BYTE *)srk.secret))) {
 		OCK_LOG_DEBUG("Tspi_Policy_SetSecret failed. rc=0x%x\n", result);
 	}
+	if (srk.secret)
+		free(srk.secret);
 
 done:
 	return result;
@@ -1595,9 +1603,9 @@ token_specific_login(CK_USER_TYPE userType, CK_CHAR_PTR pPin, CK_ULONG ulPinLen)
 
 		rc = load_private_token_objects();
 
-		XProcLock( xproclock );
+		XProcLock();
 		global_shm->priv_loaded = TRUE;
-		XProcUnLock( xproclock );
+		XProcUnLock();
 	} else {
 		/* SO path --
 		 */
